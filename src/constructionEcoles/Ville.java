f03c4eb09b992fd36b871ea1b9c55e65256fef80
thanh vo.u-paris <61943753+congvtc32012@users.noreply.github.com>
2020-10-29 20:47:25 +0100

    Update Ville.java

--- src/constructionEcoles/Ville.java
+++ src/constructionEcoles/Ville.java
@@ -1,5 +1,35 @@
 package constructionEcoles;
+import java.util.ArrayList;
 
 public class Ville {
+	
+	private Boolean hasEcole;
+	private ArrayList<Ville> voisins;
+	private String key;
+	
+	public Ville() {
+		this.hasEcole = hasEcole;
+		this.voisins = voisins;
+		this.key= key;
+		
+	}
+	
+	public boolean getHasEcole() {
+		  return hasEcole;
+	}
+	
+	public void setHasEcole() {
+		 this.hasEcole = hasEcole;
+	}
+	
+	public String getKey() {
+		return key;
+	}
+	
+	public boolean hasEcoleVoisins() {
+		  
+	}
+	
+     
 
 }
